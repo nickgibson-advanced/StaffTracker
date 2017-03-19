c465ff28910f38873d089d814d6b8e409613d73c
Nick Gibson <nick.gibson@oneadvanced.com>
2017-03-19 10:29:35 +0000

    first round

--- StaffTracker/Controllers/HomeController.cs
+++ StaffTracker/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace StaffTracker.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
